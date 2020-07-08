5985ea30854fdeffc2f83dfc29eaa16692c1747b
Roy Golan <rgolan@redhat.com>
2020-07-08 17:56:12 +0300

    Standard and more concise version info
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,13 +1,30 @@
 package version
 
-import (
-	"fmt"
-	"runtime"
-)
+import "k8s.io/apimachinery/pkg/version"
 
 var (
-	Version    = "v0.1.0"
-	GitVersion = "unknown"
-	GitCommit  = "unknown"
-	GoVersion  = fmt.Sprintf("%s %s/%s", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	// commitFromGit is a constant representing the source version that
+	// generated this build. It should be set during build via -ldflags.
+	commitFromGit string
+	// versionFromGit is a constant representing the version tag that
+	// generated this build. It should be set during build via -ldflags.
+	versionFromGit string
+	// major version
+	majorFromGit string
+	// minor version
+	minorFromGit string
+	// build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
+	buildDate string
 )
+
+// Get returns the overall codebase version. It's for detecting
+// what code a binary was built from.
+func Get() version.Info {
+	return version.Info{
+		Major:      majorFromGit,
+		Minor:      minorFromGit,
+		GitCommit:  commitFromGit,
+		GitVersion: versionFromGit,
+		BuildDate:  buildDate,
+	}
+}
